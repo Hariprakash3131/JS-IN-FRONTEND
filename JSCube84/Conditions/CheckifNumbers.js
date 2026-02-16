db14e2accbda576963d3de3500c48133760ea2e2
Hariprakash3131 <hariprakash3131@gmail.com>
2026-02-16 23:51:01 +0530

    js-9

---
+++ JSCube84/Conditions/CheckifNumbers.js
@@ -0,0 +1,13 @@
+let num=1
+
+if(num>=1)
+    console.log('Positive')
+else if(num<0)(
+    console.log('Negative')
+)
+else if(num==0){
+    console.log('Zero')
+}
+else{
+    console.log('Code Wrong')
+}
