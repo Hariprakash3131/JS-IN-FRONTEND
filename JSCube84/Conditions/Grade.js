2b12d2a6a1d1f46f296c5e806995a33ead251fb4
Hariprakash3131 <hariprakash3131@gmail.com>
2026-02-16 23:40:19 +0530

    js-7

---
+++ JSCube84/Conditions/Grade.js
@@ -0,0 +1,14 @@
+let marks=67
+
+if(marks>=90){
+    console.log('Grade A')
+}
+else if(marks>=89||75){
+    console.log('Grade B')
+}
+else if(marks>=74||50){
+    console.log('Grade C')
+}
+else{
+    console.log('Grade C')
+}
