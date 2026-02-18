b6c456d76989d6bba45e9ea7e1224571afd26ccf
Hariprakash3131 <hariprakash3131@gmail.com>
2026-02-18 11:58:34 +0530

    js-19

---
+++ Cube84Task-Js/findLarge3Numbers.js
@@ -0,0 +1,14 @@
+let num1=45
+let num2=56
+let num3=67
+
+if(num1>=num2 && num1>=num3)
+{
+    console.log('Largest Number:'+num1)
+}
+else if(num2>=num1 && num2>=num3){
+    console.log('Largest Number:'+num2)
+}
+else{
+    console.log('Largest Number:'+num3)
+}
