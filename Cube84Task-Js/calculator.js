7ef6557dfd3327a53c4290cfbb5d10a648ea8a3e
Hariprakash3131 <hariprakash3131@gmail.com>
2026-02-18 09:59:02 +0530

    js-19

---
+++ Cube84Task-Js/calculator.js
@@ -0,0 +1,23 @@
+let num1=10
+let num2=20
+let p=num1+num2
+let n=num1-num2
+let d=num1/num2
+let m=num1*num2
+let operator=m
+
+if(operator==p){
+    console.log(p)
+}
+else if(operator==n){
+    console.log(n)
+}
+else if(operator==d){
+    console.log(d)
+}
+else if(operator==m){
+    console.log(m)
+}
+else {
+    console.log('Not a Valid Number')
+}
