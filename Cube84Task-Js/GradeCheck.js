ebd68b599cf7af3c67e69a24b83c33b578b8f9d4
Hariprakash3131 <hariprakash3131@gmail.com>
2026-02-18 09:48:46 +0530

    js-16

---
+++ Cube84Task-Js/GradeCheck.js
@@ -0,0 +1,18 @@
+let marks=37
+
+if(marks>=90)
+{
+    console.log('A Grade')
+}
+else if(marks>=75){
+    console.log("B Grade")
+}
+else if(marks>=60){
+    console.log("C Grade")
+}
+else if(marks>=40){
+    console.log('D Grade')
+}
+else{
+    console.log('Fail')
+}
