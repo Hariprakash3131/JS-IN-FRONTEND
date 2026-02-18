7de65c0bee5e0ecc3bf122375541b4078f6f09cc
Hariprakash3131 <hariprakash3131@gmail.com>
2026-02-18 09:44:53 +0530

    js-15

--- Cube84Task-Js/OddorEven.js
+++ Cube84Task-Js/OddorEven.js
@@ -3,5 +3,5 @@ if(num%2==0){
     console.log(num +" is Even number")
 }
 else{
-    console.log(num+" is a Odd number")
+    console.log(num +" is a Odd number")
 }
---
+++ Cube84Task-Js/postiveAndNegativeZero.js
@@ -0,0 +1,14 @@
+let a=-1
+
+if(a>=1){
+    console.log("Positive")
+}
+else if(a<0){
+    console.log('Negative')
+}
+else if(a==0){
+    console.log('Zero')
+}
+else{
+    console.log('Not a Valid number')
+}
