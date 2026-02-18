bd9c50350555a423f2e988325754013e63e0d81a
Hariprakash3131 <hariprakash3131@gmail.com>
2026-02-18 12:16:05 +0530

    js-20

---
+++ Cube84Task-Js/evenOddCount.js
@@ -0,0 +1,14 @@
+let a=50
+let even=0;
+let odd=0;
+for(let i=1;i<=a;i++){
+    if(i%2==0){
+       even++
+    }
+    else{
+        odd++
+    }
+    
+}
+console.log('Total Even Number:'+even)
+console.log('Total Odd Number is:'+odd)
