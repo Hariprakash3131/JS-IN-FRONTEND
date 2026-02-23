043a22f770f36f5f84015fb9925571a64c4b6334
Hariprakash3131 <hariprakash3131@gmail.com>
2026-02-23 23:44:44 +0530

    js-40

---
+++ JSCube84/Logic/Day-1/functionwithpalli.js
@@ -0,0 +1,11 @@
+function palindrome(a){
+    let crt=a
+    let reverse=0
+    while(a>0){
+        let lastNum=a%10
+        reverse=reverse*10+lastNum
+        a=Math.floor(a/10)
+    }
+    return crt === reverse
+}
+console.log(palindrome(121))
