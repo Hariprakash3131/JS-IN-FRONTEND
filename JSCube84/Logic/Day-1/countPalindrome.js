764eb492f5d090ee932aaf31518c24216283e485
Hariprakash3131 <hariprakash3131@gmail.com>
2026-02-24 23:08:20 +0530

    js-45

---
+++ JSCube84/Logic/Day-1/countPalindrome.js
@@ -0,0 +1,12 @@
+
+for(let i=1;i<=200;i++){
+    let palindrome=0
+let reverse=0
+    let lastNumber=i%10
+    reverse=reverse*10+lastNumber
+    i=Math.floor(i/10)
+}
+return reverse
+if(palindrome===reverse){
+   console.log( palindrome++)
+}
---
+++ JSCube84/Logic/Day-1/factorial.js
@@ -0,0 +1,7 @@
+let fact=5
+let ans=1
+for (let i=1;i<=fact;i++){
+    ans*=i
+    
+}
+console.log(ans)
