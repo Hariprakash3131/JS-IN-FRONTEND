1d57646737a7f577ee52064fa1a164976183f36c
Hariprakash3131 <hariprakash3131@gmail.com>
2026-02-24 23:10:37 +0530

    js-46

---
+++ JSCube84/Logic/Day-1/factWithFunction.js
@@ -0,0 +1,8 @@
+function fact(num){
+    let ans=1
+    for(let i=1;i<=num;i++){
+        ans*=i
+    }
+  return ans
+}
+console.log(fact(5))
--- JSCube84/Logic/Day-1/factorial.js
+++ JSCube84/Logic/Day-1/factorial.js
@@ -2,6 +2,5 @@ let fact=5
 let ans=1
 for (let i=1;i<=fact;i++){
     ans*=i
-    
 }
 console.log(ans)
