26c6903aedca0f00b96151710fd131a7aa35812f
Hariprakash3131 <hariprakash3131@gmail.com>
2026-02-25 17:15:07 +0530

    js-47

---
+++ JSCube84/ArrayMethods/push.js
@@ -0,0 +1,11 @@
+const arr=['Apple','car','tata']
+arr.push('auto')
+console.log(arr)
+
+const name=['bat','ball','stump']
+name.push('glove')
+console.log(name)
+
+const num=[1,2,3,4,5]
+num.push(6)
+console.log(num)
