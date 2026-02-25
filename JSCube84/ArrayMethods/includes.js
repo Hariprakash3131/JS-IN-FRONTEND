d4a392d8784e1b7fce82668a290e8814fa2a66ea
Hariprakash3131 <hariprakash3131@gmail.com>
2026-02-25 17:33:40 +0530

    js-51

---
+++ JSCube84/ArrayMethods/includes.js
@@ -0,0 +1,10 @@
+const arr=[1,2,4,5,6,7,7]
+console.log(arr.includes(2))
+console.log(arr.includes(7))
+console.log(arr.includes(9))
+
+
+const name=['aus','india','sa']
+console.log(name.includes('india'))
+console.log(name.includes('aus'))
+console.log(name.includes('nz'))
