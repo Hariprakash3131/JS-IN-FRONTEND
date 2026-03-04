352549f6a2c30f6d36f76c19cc71b367362b44f4
Hariprakash3131 <hariprakash3131@gmail.com>
2026-03-04 13:14:50 +0530

    js-60

---
+++ Cube84Task-Js/Objects/arrayOfusers.js
@@ -0,0 +1,21 @@
+const users=[
+    {id:123,name:'Kumar',isActive:true},
+    {id:1234,name:'Sarath',isActive:false},
+    {id:12345,name:'Sathish',isActive:true}
+]
+
+users.forEach(user=>{
+    if(user.isActive){
+        console.log(user)  
+    }
+})
+
+
+let count=0
+users.forEach(user=>{
+    if(user.isActive===false){
+        count++
+    }
+})
+
+console.log('Total User Active:',count)
