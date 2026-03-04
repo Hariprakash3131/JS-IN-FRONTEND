76d80623684d3470f4ca303eed1e1cba44c14143
Hariprakash3131 <hariprakash3131@gmail.com>
2026-03-04 12:27:14 +0530

    js-57

--- JSCube84/Objects/objectsExample.js
+++ JSCube84/Objects/objectsExample.js
@@ -6,3 +6,14 @@ const car={
 console.log(car)
 console.log(car.model)
 console.log(car.name)
+
+
+const person={};
+
+person.name="Sarath"
+person.lastName='Kumar'
+person.age='27'
+
+console.log(person.name)
+console.log(person.name+" "+person.lastName)
+console.log(person.age)
