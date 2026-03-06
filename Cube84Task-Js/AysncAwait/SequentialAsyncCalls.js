95dc3d95a7e95b8add7be93e8becbf9c7590919b
Hariprakash3131 <hariprakash3131@gmail.com>
2026-03-06 23:51:27 +0530

    js-75

--- Cube84Task-Js/AysncAwait/SequentialAsyncCalls.js
+++ Cube84Task-Js/AysncAwait/SequentialAsyncCalls.js
@@ -23,3 +23,32 @@ async function getData(){
 }
 
 getData()
+
+
+function fetchUsers(){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("User Data Fetch")
+        },2000)
+    })
+}
+
+function fetchOrders(){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve('Order Data Fetch')
+        },2000)
+    })
+}
+
+async function getData(){
+    let user=await fetchUsers()
+    console.log(user)
+
+    let order=await fetchOrders()
+    console.log(order)
+}
+
+getData()
+
+
