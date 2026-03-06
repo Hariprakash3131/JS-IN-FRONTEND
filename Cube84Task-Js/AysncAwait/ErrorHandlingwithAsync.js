f25099268e1cb6f64ba38b65220d652794cc08ef
Hariprakash3131 <hariprakash3131@gmail.com>
2026-03-06 15:06:35 +0530

    js-73

---
+++ Cube84Task-Js/AysncAwait/ErrorHandlingwithAsync.js
@@ -0,0 +1,26 @@
+function errorHandling(){
+   return new Promise((resolve,reject)=>{
+    let a=9
+    
+    if(a===5){
+        resolve('Success')
+    }
+    else{
+        reject('Something Went Wrong')
+    }
+})
+}
+
+async function errorNoticed(){
+    try
+    {           
+        let output=await errorHandling()
+        console.log(output)
+    }
+
+    catch(error){
+        console.log(error)
+    }
+}
+
+errorNoticed()
