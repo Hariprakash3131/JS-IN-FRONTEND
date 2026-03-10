ee78ca6bce794a72c9f0106f14dd8969bf646cb1
Hariprakash3131 <hariprakash3131@gmail.com>
2026-03-10 22:01:53 +0530

    js-84

---
+++ Cube84Task-Js/Task-Manager/utils.js
@@ -0,0 +1,20 @@
+let usersId=1
+let tasksId=1
+
+function createUsersId(){
+    return usersId++
+}
+
+function createTasksId(){
+    return tasksId++
+}
+
+function createDate(){
+    return new Date()
+}
+
+module.exports={
+    createUsersId,
+    createTasksId,
+    createDate
+}
