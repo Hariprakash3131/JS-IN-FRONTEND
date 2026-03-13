58c9e41273a5325ec8cb69199c482fc38567fa28
Hariprakash3131 <hariprakash3131@gmail.com>
2026-03-14 00:26:52 +0530

    js-93

--- Cube84Task-Js/Project-Plan/products.js
+++ Cube84Task-Js/Project-Plan/products.js
@@ -9,6 +9,30 @@ function productsDetails(proName,proPrice,proCategory,proStock){
         stock:proStock,
 
     }
-    products.push(productsDetails)
+    products.push(product)
     console.log('Product Added:',proName)
 }
+
+function removeProducts(){
+  let remove=products.find(p=>p.id===2)
+   products=products.filter((p)=>{
+    return p.id!==2
+   })
+    console.log("Product with id 1 removed",remove.name)
+    
+}
+
+function listProducts(){
+    console.log("Products Details:")
+    console.log(products)
+}
+
+productsDetails('Laptop',50000,'Ele',12)
+productsDetails('Mobile',50000,'Ele',12)
+productsDetails('IonBox',50000,'Ele',12)
+
+listProducts()
+
+removeProducts()
+
+listProducts()
