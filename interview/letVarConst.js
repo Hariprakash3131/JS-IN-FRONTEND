6441f12880668a478f9e504b63f1a9bd93e1e319
Hariprakash3131 <hariprakash3131@gmail.com>
2026-04-12 19:00:24 +0530

    react-26

--- interview/letVarConst.js
+++ interview/letVarConst.js
@@ -5,5 +5,35 @@ if(true){
 }
 
 console.log(x)  //10
-console.log(y)  //Reference Error
-console.log(z)   //Reference Error
+//console.log(y)  //Reference Error
+//console.log(z)   //Reference Error
+
+
+for(var i=0;i<3;i++){
+    setTimeout(()=>{
+        console.log("var:",i)
+    },0)
+}
+
+// var: 3
+// var: 3
+// var: 3
+
+
+for(let j=0;j<4;j++){
+    setTimeout(()=>{
+        console.log('Let:',j)
+    },2000)
+}
+
+
+// Let: 0
+// Let: 1
+// Let: 2
+// Let: 3
+
+
+const user={name:"Sarath"}
+
+user.name="Surya"
+console.log(user.name)  //surya
