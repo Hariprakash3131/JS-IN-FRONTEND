d5e522459bea5da852d7b2ef41e38632b72ff4c2
Hariprakash3131 <hariprakash3131@gmail.com>
2026-04-13 17:06:44 +0530

    react-30

--- interview/arrowFunction.js
+++ interview/arrowFunction.js
@@ -1,4 +1,25 @@
 const add=(a,b)=>{
     return a+b
-}
+}                             //When you use curly brackets {} inside arrow function, you must write return.
 console.log((add(10,20)))
+
+
+const sub=(s,o)=>{
+    console.log(s-o)
+}      
+
+sub(30,20)
+
+
+const sarath=(q,w)=>q+w
+
+console.log(sarath(10,200))
+
+
+const mul=(e,r)=>{
+    e*r
+}
+
+console.log(mul(10,4))  //undefined
+
+// When you use curly brackets {} inside arrow function, you must write return.
