12183434dbab02af0f659d3288877349122d5d04
Hariprakash3131 <hariprakash3131@gmail.com>
2026-04-14 12:37:24 +0530

    react-31

---
+++ interview/function.js
@@ -0,0 +1,12 @@
+function greet(name){
+    console.log('Hello:',name)
+}
+greet('Deva')
+
+function person(name){   //name  is a parameter
+    console.log('Hello:',name)
+}
+
+person('Surya')  // surya is arguments
+person('Subash') //Subash is arguments
+
